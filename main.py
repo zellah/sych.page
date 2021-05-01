24d237a40e50904a0939ec718c2377887141d3f1
Nat Henderson <nath@nmckinley.com>
2021-04-30 23:06:51 -0700

    make work on windows and also add userdata object.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
+from datetime import datetime
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\hende\\AppData\\Roaming\\mydatabase.db'
+if os.name == 'nt':
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\hende\\AppData\\Roaming\\mydatabase.db'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/db.db'
 db = SQLAlchemy(app)
 
 @app.route("/")
@@ -21,6 +27,17 @@ def search():
 def all_users():
     return str(User.query.all())
 
+@app.route('/redirect')
+def new_user():
+    user = User(data=UserData())
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('user', id=user.id))
+
+@app.route('/user/<id>')
+def user(id):
+    return str(User.query.get(id).data)
+
 members = db.Table('members',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True)
@@ -33,5 +50,42 @@ class User(db.Model):
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     members = db.relationship(User, secondary=members, lazy='subquery', backref=db.backref('groups', lazy=True))
+
+class Doorsness(Enum):
+    OUTDOORS=0
+    WHO_CARE=1
+
+class Maskedness(Enum):
+    MASKED=0
+    WHO_CARE=1
+
+class Distanced(Enum):
+    DISTANCED=0
+    WHO_CARE=1
+
+class Vaccinated(Enum):
+    ALL=0
+    ALL_BUT_ONE=1
+    WHO_CARE=2
+
+class Food(Enum):
+    NO=0
+    BRING_OWN=1
+    WHO_CARE=2
+
+class UserData(object):
+    def __init__(self):
+        self._data = [False] * (len(Doorsness) * len(Maskedness) * len(Distanced) * len(Vaccinated) * len(Food))
+        self.vaccinated_date = datetime.max
+
+    def ok_with(self, doorsness, maskedness, distanced, vaccine, food):
+        return self._data[
+            doorsness * (len(Maskedness) * len(Distanced) * len(Vaccinated) * len(Food)) + maskedness * (len(Distanced) * len(Vaccinated) * len(Food)) + distanced * (len(Vaccinated) * len(Food)) + vaccine * len(Food) + food]
+
+    def set_ok(self, doorsness, maskedness, distanced, vaccine, food, ok):
+        self._data[doorsness * (len(Maskedness) * len(Distanced) * len(Vaccinated) * len(Food)) + maskedness * (len(Distanced) * len(Vaccinated) * len(Food)) + distanced * (len(Vaccinated) * len(Food)) + vaccine * len(Food) + food] = ok
+
+    def __str__(self):
+        return str(self._data) + str(self.vaccinated_date)
 
 db.create_all()
