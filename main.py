d9bcfae2c9925b88d6974cd805c41dce89247ffe
Zella <2789195+zellah@users.noreply.github.com>
2021-04-30 22:09:29 -0700

    Very bad webpages exist now

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template('home.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/search")
+def search():
+    return render_template('search.html')
