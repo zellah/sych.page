e23e4b81ff482d6dfc8c8c57175d212449b7754e
Nat Henderson <nath@nmckinley.com>
2021-04-30 23:46:53 -0700

    allow mutation of okness

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from enum import Enum
 from datetime import datetime
 import os
+from sqlalchemy.ext.mutable import Mutable
 
 app = Flask(__name__)
 if os.name == 'nt':
@@ -37,6 +38,35 @@ def new_user():
 @app.route('/user/<id>')
 def user(id):
     return str(User.query.get(id).data)
+
+@app.route('/user/<id>/set_ok')
+def set_okness(id):
+    doorsness=Doorsness[request.args.get('doorsness')]
+    maskedness=Maskedness[request.args.get('maskedness')]
+    distanced=Distanced[request.args.get('distanced')]
+    vaccinated=Vaccinated[request.args.get('vaccinated')]
+    food=Food[request.args.get('food')]
+    user = User.query.get(id)
+    user.data = user.data.with_ok(doorsness, maskedness, distanced, vaccinated, food, True)
+    db.session.add(user)
+    print(user.data.ok_with(doorsness, maskedness, distanced, vaccinated, food))
+    db.session.commit()
+    print(user.data.ok_with(doorsness, maskedness, distanced, vaccinated, food))
+    return redirect(url_for('user', id=user.id))
+
+
+@app.route('/user/<id>/set_not_ok')
+def set_not_okness(id):
+    doorsness=Doorsness[request.args.get('doorsness')]
+    maskedness=Maskedness[request.args.get('maskedness')]
+    distanced=Distanced[request.args.get('distanced')]
+    vaccinated=Vaccinated[request.args.get('vaccinated')]
+    food=Food[request.args.get('food')]
+    user = User.query.get(id)
+    user.data = user.data.with_ok(doorsness, maskedness, distanced, vaccinated, food, False)
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('user', id=user.id))
 
 members = db.Table('members',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
@@ -73,17 +103,27 @@ class Food(Enum):
     BRING_OWN=1
     WHO_CARE=2
 
-class UserData(object):
+class UserData(Mutable):
     def __init__(self):
         self._data = [False] * (len(Doorsness) * len(Maskedness) * len(Distanced) * len(Vaccinated) * len(Food))
         self.vaccinated_date = datetime.max
 
     def ok_with(self, doorsness, maskedness, distanced, vaccine, food):
         return self._data[
-            doorsness * (len(Maskedness) * len(Distanced) * len(Vaccinated) * len(Food)) + maskedness * (len(Distanced) * len(Vaccinated) * len(Food)) + distanced * (len(Vaccinated) * len(Food)) + vaccine * len(Food) + food]
+            doorsness.value * (len(Maskedness) * len(Distanced) * len(Vaccinated) * len(Food)) + maskedness.value * (len(Distanced) * len(Vaccinated) * len(Food)) + distanced.value * (len(Vaccinated) * len(Food)) + vaccine.value * len(Food) + food.value]
 
-    def set_ok(self, doorsness, maskedness, distanced, vaccine, food, ok):
-        self._data[doorsness * (len(Maskedness) * len(Distanced) * len(Vaccinated) * len(Food)) + maskedness * (len(Distanced) * len(Vaccinated) * len(Food)) + distanced * (len(Vaccinated) * len(Food)) + vaccine * len(Food) + food] = ok
+    def with_ok(self, doorsness, maskedness, distanced, vaccine, food, ok):
+        ud = UserData()
+        ud.vaccinated_date = self.vaccinated_date
+        ud._data = self._data
+        ud._data[doorsness.value * (len(Maskedness) * len(Distanced) * len(Vaccinated) * len(Food)) + maskedness.value * (len(Distanced) * len(Vaccinated) * len(Food)) + distanced.value * (len(Vaccinated) * len(Food)) + vaccine.value * len(Food) + food.value] = ok
+        return ud
+
+    def with_vaccinated_date(self, date)
+        ud = UserData()
+        ud._data = self._data
+        ud.vaccinated_date = date
+        return ud
 
     def __str__(self):
         return str(self._data) + str(self.vaccinated_date)
